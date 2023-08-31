188597e1137c612ffcc7109b97442613d0330952
lucas wanderosck <lucaskleinwanderosck@gmail.com>
2023-08-31 14:22:58 -0300

    testando a adição de dois comentários

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,23 @@
-import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
-import PostComment from '.';
+import { fireEvent, render, screen } from "@testing-library/react";
+import Post from ".";
 
-describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
-        render(<PostComment/>);
-        expect(screen.getByText('Comentar')).toBeInTheDocument();
+describe("testes para o componente PostComments", () => {
+  test("deve renderizar o componente corretamente", () => {
+    render(<Post />);
+    expect(screen.getByText("Comentar")).toBeInTheDocument();
+  });
+
+  test("deve adicionar os comentários", () => {
+    render(<Post />);
+    fireEvent.change(screen.getByTestId("post-comment-input"), {
+      target: { value: "primeiro comentário" },
     });
+    fireEvent.click(screen.getByTestId("btn-post-comment"));
+
+    fireEvent.change(screen.getByTestId("post-comment-input"), {
+      target: { value: "segundo comentário" },
+    });
+    fireEvent.click(screen.getByTestId("btn-post-comment"));
+    expect(screen.getAllByTestId("list-comment")).toHaveLength(2);
+  });
 });
